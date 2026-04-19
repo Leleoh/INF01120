4a33746da64f2f1bf50384f5c7ec24eb92dde301
Leonel Hernandez <leonelfhernandez@outlook.com>
2026-04-19 01:44:36 -0300

    Header do MusicContext criado com variáveis e funções

--- Peppertune/include/MusicContext.h
+++ Peppertune/include/MusicContext.h
@@ -1,1 +1,22 @@
 //Arquivo de interface do MusicContext
+
+class MusicContext{
+    private:
+    int bpm;
+    int volume;
+    int defaultOctave;
+    int currentOctave;
+    int currentInstrument;
+
+    public:
+    MusicContext();
+
+    void resetToDefault();
+    void raiseOctave();
+    void doubleVolume();
+
+    int getBpm() const;
+    int getVolume() const;
+    int getCurrentOctave() const;
+    int getCurrentInstrument() const;
+};
