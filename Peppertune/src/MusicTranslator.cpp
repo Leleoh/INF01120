ce29220174a92a315d4cf8c985ae1c8e1f0adcb1
Leonel <leonelfhernandez@outlook.com>
2026-04-23 11:09:16 -0300

    Criando mapeamento dos caracteres

---
+++ Peppertune/src/MusicTranslator.cpp
@@ -0,0 +1,50 @@
+#include "MusicTranslator.h"
+
+//Função responsável por chamar as funções do MusicContext a partir dos parâmetros
+void MusicTranslator::applyMapping(char letter, MusicContext& currentVoice, int& globalBPM){
+
+    switch (letter)
+    {
+    case '?':
+        currentVoice.raiseOctave();
+        break;
+    
+    case 'V':
+        currentVoice.lowerOctave();
+        break;
+
+    case ' ':   //Espaço
+        currentVoice.doubleVolume();
+        break;
+
+    case '!':
+        currentVoice.setInstrument(22); //Harmonica
+        break;
+
+    case ';':
+        currentVoice.setInstrument(15); //Tubular bells
+        break;
+
+    case ',':
+        currentVoice.setInstrument(20); //Church organ
+        break;
+
+
+    //Controles de BPM
+    case '<':
+        globalBPM += 10;
+        break;
+    
+    case '>':
+        globalBPM -= 10;
+        break;
+
+    //Caracteres para notas e pausas (FUTURAMENTE)
+    
+        
+    default:    //Vogais e letras não mapeadas
+        break;
+    }
+
+
+}
