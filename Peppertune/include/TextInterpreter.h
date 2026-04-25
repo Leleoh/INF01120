a7c03b2b7a82f326a61b9d677addbe54b6b9d18d
Leonel <leonelfhernandez@outlook.com>
2026-04-25 18:57:27 -0300

    Implementa a classe TextInterpreter para ler os .txt

--- Peppertune/include/TextInterpreter.h
+++ Peppertune/include/TextInterpreter.h
@@ -1,1 +1,29 @@
 //Arquivo de interface do TextInterpreter
+#ifndef TEXTINTERPRETER_H
+#define TEXTINTERPRETER_H
+
+#include <string>
+#include <vector>
+#include "MusicContext.h"
+
+class TextInterpreter{
+    private:
+
+    int globalBPM;
+
+    //Array dinâmico que guarda as vozes (linhas)
+    std::vector<MusicContext> voices;
+
+    public:
+    TextInterpreter();
+
+    //Função principal que abre o arquivo e lê o texto
+    void parseFile(const std::string& filepath);
+
+    //Getters para gerar áudio
+    int getGlobalBPM() const;
+    std::vector<MusicContext> getVoices() const;
+
+};
+
+#endif
--- Peppertune/src/TextInterpreter.cpp
+++ Peppertune/src/TextInterpreter.cpp
@@ -1,1 +1,51 @@
 //Arquivo inicial de lógica de interpretação de texto
+
+#include "TextInterpreter.h"
+#include "MusicTranslator.h"
+#include <fstream>
+#include <iostream>
+
+TextInterpreter::TextInterpreter(){
+    globalBPM = 120; //Garante que começa com 120
+}
+
+void TextInterpreter::parseFile(const std::string& filepath){
+    
+    //Tenta abrir o arquiv .txt
+    std::ifstream file(filepath);
+
+    //Erro de abertura
+    if (!file.is_open()){
+        std::cerr << "Erro: Não foi possível abrir o arquivo" << filepath << std::endl;
+        return;
+    }
+
+    std::string currentLine;
+    int voiceID = 0;
+
+    //Analisa o texto até achar um \n, vai jogando dentro do currentLine
+    while (std::getline(file, currentLine)){
+
+        //Instancia uma nova voz
+        MusicContext newVoice(voiceID);
+
+        //Chama o tradutor para aplicar a função definida do mapeamento
+        for (char letter: currentLine){
+            MusicTranslator::applyMapping(letter, newVoice, globalBPM);
+        }
+
+        voices.push_back(newVoice);
+
+        voiceID++;
+    }
+
+    file.close();
+}
+
+int TextInterpreter::getGlobalBPM() const{
+    return globalBPM;
+}
+
+std::vector<MusicContext> TextInterpreter::getVoices() const{
+    return voices;
+}
